9f7c28d51cc3e8f77e6e4a4924914f639bf800e7
Ernesto de Oliveira <ernestodeoliveira@gmail.com>
2022-09-19 14:45:01 -0300

    alterações mapas

--- js/localapi.js
+++ js/localapi.js
@@ -13,8 +13,8 @@ const encuentrame = () => {
         //mapa
         // https://www.mapquestapi.com/staticmap/v5/map?key=KEY&locations=LOCATION&size=WIDTH,HEIGHT
         const mapa = new Image();
-        mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=umf0OymiBaMtJ3giTZBcOha7x4IAmVpH&locations=${latitude},${longitude}&size=400,300`;
-        //mapa.src = `https://www.mapquestapi.com/directions/v2/route?key=KEY&from=florianopolis%2C+sc&to=s%C3%A3o+paulo%2C+sp&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false`;
+        //mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=umf0OymiBaMtJ3giTZBcOha7x4IAmVpH&locations=${latitude},${longitude}&size=400,300`;
+        mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=umf0OymiBaMtJ3giTZBcOha7x4IAmVpH&start=${latitude},${longitude}&end=Av. Paulista, São Paulo - SP&routeArc=true&size=700,400`;
         divMapa.appendChild(mapa);
     };
 
